6b230fd718a649f8793665291858cf8d1ab22f05
alpha-beta-soup <richard.m.law@gmail.com>
2025-11-25 16:21:56 +1300

    more structural changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -24,36 +24,25 @@ export default defineConfig({
           label: 'Classification (v0.3)',
           collapsed: true,
           items: [
-            { label: 'Classification system', link: '/classification/system' },
-            { label: 'Classes', link: '/classification/v0-3/classes' },
-            { label: 'Class 1.0.0', link: '/classification/v0-3/class-1' },
-            { label: 'Class 2.0.0', link: '/classification/v0-3/class-2' },
-            { label: 'Class 3.0.0', link: '/classification/v0-3/class-3' },
-            { label: 'Data structure', link: '/classification/v0-3/structure' },
-            { label: 'Tenure', link: '/classification/v0-3/tenure' },
-            { label: 'Scale & temporality', link: '/classification/v0-3/scale' },
-            { label: 'Format', link: '/classification/v0-3/format' },
+            'classification/v0-3/system',
+            'classification/v0-3/structure',
+            'classification/v0-3/scale',
+            'classification/v0-3/format',
+            'classification/v0-3/tenure',
+            {
+              label: 'Classes',
+              autogenerate: {directory: 'classification/v0-3/classes', collapsed: true},
+              collapsed: false,
+            }
           ],
         },
-        // {
-        //   label: 'Classification (v0.4)',
-        //   collapsed: true,
-        //   items: [
-        //     { label: 'Classification system', link: '/classification/v0-4/system' },
-        //     { label: 'Classes', link: '/classification/v0-4/classes' },
-        //     { label: 'Class 1.0.0', link: '/classification/v0-4/class-1' },
-        //     { label: 'Class 2.0.0', link: '/classification/v0-4/class-2' },
-        //     { label: 'Class 3.0.0', link: '/classification/v0-4/class-3' },
-        //     { label: 'Data structure', link: '/classification/v0-4/structure' },
-        //     { label: 'Tenure', link: '/classification/v0-4/tenure' },
-        //     { label: 'Scale & temporality', link: '/classification/v0-4/scale' },
-        //     { label: 'Format', link: '/classification/v0-4/format' },
-        //   ],
-        // },
         {
           label: 'About us',
           items: [
-            { label: 'Steering Group (NZLUSG)', link: '/governance/governance' },
+            {
+              label: 'Governance',
+              autogenerate: {directory: 'governance/NZLUSG'}
+            },
             { label: 'Contact us', link: '/governance/contact' },
           ],
         },
